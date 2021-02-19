dd7e63071c91d4a3c3af2ef830c44d83f85ef46e
vamsitechtuts <sudhamvc@gmail.com>
2021-02-19 06:28:49 +0530

    added springboot project

---
+++ src/main/java/com/companyname/devops6ambatchdemo/Devops6amBatchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.companyname.devops6ambatchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops6amBatchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops6amBatchDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/companyname/devops6ambatchdemo/Devops6amBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companyname.devops6ambatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops6amBatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
